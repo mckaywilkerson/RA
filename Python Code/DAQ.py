976098980ce671d5f8783d637cd39f6245c9a365
McKay <mckaywilkerson@gmail.com>
2019-09-18 13:23:00 -0600

    9/18

--- Python Code/DAQ.py
+++ Python Code/DAQ.py
@@ -1,22 +1,13 @@
-import visa
-import time
+#import time
 import nidaqmx
 #import nidaqmx.system
-
-rm = visa.ResourceManager()
-print(rm.list_resources())
 
 system = nidaqmx.system.System.local()
 for device in system.devices:
     print(device)
 
-#with nidaqmx.Task() as task:
-#    task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
-#    print(task.read())
-
-#my_instrument = rm.open_resource('ASRL6::INSTR')
-
-#my_instrument.read_termination = '\r'
-#my_instrument.write_termination = '\r'
-
-#print(my_instrument.read())
+while (1):
+    with nidaqmx.Task() as task:
+        task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+        temp = float(task.read()) * 16.562 - 12.369
+        print(temp)
--- Python Code/PID control.py
+++ Python Code/PID control.py
@@ -1,7 +1,12 @@
+# NOTES: make sure that you know that VISA port for the power supply before you start this (line 45)
+#        also need to make sure that the DAQ is Dev1 (line 31)
+
 import PID
 import time
 import nidaqmx
+import visa
 
+system = nidaqmx.system.System.local()
 targetT = 20        # Target temperature
 P = 0               # Initial PID values
 I = 0
@@ -21,16 +26,30 @@ def setValues():
 while 1:
     setValues()
 
-    #READ TEMP DATA
-    temperature = 15
+    #READS TEMP DATA FROM DAQ
+    with nidaqmx.Task() as task:
+        task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+        temp = float(task.read()) * 16.562 - 12.369
 
-    pid.update(temperature, time.time())
+    pid.update(temp, time.time())
     pidOutput = pid.output
     pidOutput = max(min( int(pidOutput), 100 ),0)       # ensures that the output is between 0-100
     pidOutput = pidOutput * 9 / 100                     # scale it down to between 0-9
 
-    print(pidOutput)
+    
 
     time.sleep(.5)
 
-    #SET THE POWER SUPPLY COMMANDS BASED ON THIS
+    #SETS POWER SUPPLY COMMANDS
+    rm = visa.ResourceManager()
+    my_instrument = rm.open_resource('ASRL6::INSTR')
+    my_instrument.read_termination = '\r'
+    my_instrument.write_termination = '\r'
+
+    my_instrument.write('SESS00')   # Sets to remote mode
+    my_instrument.write('SOVP00050')# Sets max voltage to 5V
+    my_instrument.write('CURR00050')# Sets current to .5 Amps
+    my_instrument.write('SOUT000')  # Turns power supply on
+    time.sleep(1)
+    my_instrument.write('SOUT001')  # Turns power supply off
+    
