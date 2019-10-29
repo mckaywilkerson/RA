d21170e22fcc94fb6a997cc5b8a658529c42d2a2
McKay Wilkerson <mckaywilkerson@gmail.com>
2019-10-29 11:57:11 -0600

    10/29

--- Python Code/Spectrometer.py
+++ Python Code/Spectrometer.py
@@ -1,9 +1,44 @@
-import usb.core
-import usb.util
+import ctypes
+import clr
+import os
+import sys
 
-# find our device
-dev = usb.core.find(idVendor=0xfffe, idProduct=0x0001)
+import default_settings
 
-# was it found?
-if dev is None:
-    raise ValueError('Device not found')
+class MySpectro:
+    # variables
+    DataBuffer = []
+    SpectralData = []
+    DarkData = []
+    WaveData = []
+
+    IntegrationPeriod = 0
+    TemperatureCompensation = 0
+    chan = 1
+
+    buffer = []
+    
+    
+    def __init__(self):
+
+        clr.AddReference('C:\\Users\\mckay\\Desktop\\RA\\spectrom.dll')
+
+        from Spectro_1 import SwWrapper
+        self.my_spectrometer = SwWrapper()
+        #my_spectrometer.Init()
+        #my_spectrometer.Close()
+
+    def initiate_spectrometer(self):
+        self.my_spectrometer.Init()
+        # get default variable values
+        # override cal coefficients w/ ones from spectrometer
+        # fill wavedata array
+        # set integration period from XTRate
+        # set timer to 1/3 integration period (check 3 times)
+        self.my_spectrometer.Rate(self.IntegrationPeriod)
+        self.my_spectrometer.Update(default_settings.scansToAvg, default_settings.xsmooth, default_settings.tempComp)
+        self.my_spectrometer.Mode(default_settings.xtrate)
+
+    #def update_spectrometer:
+
+    #def close_spectrometer:
---
+++ Python Code/default_settings.py
@@ -0,0 +1,29 @@
+# default settings for the spectrometer
+
+# xsmooth: smoothing using pixel boxcar averaging across spectral scan
+# 0 = NONE
+# 1 = 5 pixels
+# 2 = 9 pixels
+# 3 = 17 pixels
+# 4 = 33 pixels
+int xsmooth = 1
+
+# tempComp: temperature compensation uses detector optical black region
+# pixels 1-12
+# 0 = NONE
+# 1 = ON
+int tempComp = 0
+
+# scansToAvg: scan averaging accumulates scans then divides by
+# scansToAvg (1-99)
+int scansToAvg = 1
+
+# 4 bis 65500 (differential scanning fluorimetry). Used to change
+# detector integration rate in ms (4-65500)
+int DSF = 30
+
+# xtrate: has something to do with integration period (IP)
+# 0 = if IP < 1, IP = 1
+# 1 = if IP < 15, IP = 15
+# 2 = if IP < 30, IP = 30
+int xtrate = 2
